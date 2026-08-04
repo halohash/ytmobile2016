8f46c76f20ed6bd3014cf64dc590a6b207d452b5
giggityfiles <giggityfiles@gmail.com>
2026-08-03 21:02:40 -0400

    edcxijcsjkhj

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,8 +1,17 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
 
-  if (url.searchParams.get("ajax") === "1" && url.pathname !== "/feed" && url.pathname !== "/_get_ads") {
-    return Response.redirect(url.origin + "/api" + url.pathname, 302);
+  if (
+    url.searchParams.get("ajax") === "1" &&
+    url.pathname !== "/feed" &&
+    url.pathname !== "/_get_ads"
+  ) {
+    url.searchParams.delete("ajax");
+
+    return Response.redirect(
+      url.origin + "/api" + url.pathname + url.search,
+      302
+    );
   }
 
   return context.next();
